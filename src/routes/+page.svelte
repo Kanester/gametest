bf6d290d0f3a4ac24f6d36dc90851cfbcac3fa18
KanesterP <pampagkieven091@gmail.com>
2025-05-20 21:52:00 +0800

    --no-message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
   let canvas: HTMLCanvasElement;
   let ctx: CanvasRenderingContext2D;
   
-  let vx, vy, x, y = 0;
+  let [vx, vy, x, y] = [0, 0, 0, 0];
   let last = performance.now();
   
   const handleMove = (dx: number, dy: number) => {
