8ce974026ca20b28f739da73b3a8e8114be3520f
KanesterP <pampagkieven091@gmail.com>
2025-05-20 20:30:38 +0800

    --no-message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { resize } from "$lib/utils.ts"
-  import Square from "$lib/entity/Square.svelte"
+  import { resize } from "$lib/utils.ts";
+  import Square from "$lib/entity/Square.svelte";
+  import Joystick from "$lib/hud/Joystick.svelte";
   
   let canvas: HTMLCanvasElement;
   let ctx: CanvasRenderingContext2D;
+  
+  const handleMove = (x: number, y: number) => {
+    console.log("Joystick: ", x.toFixed(2), y.toFixed(2));
+  }
+  
   const res = () => {
     resize(canvas, ctx);
   }
@@ -16,13 +22,14 @@
     return () => {
       window.removeEventListener("resize", res);
     }
-  })
+  });
 </script>
 
 <canvas bind:this={canvas}></canvas>
+<Joystic size={100} onMove={handleMove} dynamic={true} />
 
 {#if ctx}
-  <Square ctx={ctx} x={30} y={20} width={10} height={10}  />
+  <Square ctx={ctx} x={30} y={20} width={10} height={10} />
 {/if}
 
 <style>
