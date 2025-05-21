c14079dd17d95fed222a2c377b09e0d3854dc584
KanesterP <pampagkieven091@gmail.com>
2025-05-21 15:06:03 +0800

    --no-message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-	import Canvas from '$lib/hud/Canvas.svelte';
+	import Canvas from '$lib/hud/Canvas.svelte';1
+	import Joystick from "$lib/hud/Joystick.svelte"
+	import { joystick } from "$lib/utils"
 	import Square from "$lib/entity/Square.svelte"
 </script>
 
 <Canvas />
 <Square />
+<Joystick />
